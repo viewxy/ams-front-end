1bb6d3197c3be5351f5e9503f4bb699afb808ed4
IstvanPongracz <pongracz88@gmail.com>
2022-02-16 14:20:59 +0100

    getImages datas, detailed artwork

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,11 @@ import http from "axios";
 import SignUp from "./components/SignUp";
 
 function App() {
-  const [museum, setMuseum] = useState([]);
+  // const [museum, setMuseum] = useState([]);
   const [imagesOnLoad, setImagesOnLoad] = useState([]);
   const [keyword, setKeyword] = useState("");
+
+  const [isShown, setIsShown] = useState(true);
 
   /*
   const loadMuseum = async () => {
@@ -31,6 +33,10 @@ function App() {
   };
   */
 
+ const showArtworkDetails = () => {
+   setIsShown((isShown) => !isShown);
+ };
+
   // Cleveland API
   const loadCleveland = async(keyword) => {
     let imageData = [];
@@ -39,8 +45,8 @@ function App() {
       q: keyword, // keyword from input
       limit: 20, // number of results
       has_image: 1 // it has an image
-    };
-
+    };  
+  
     const getImages = await http("https://openaccess-api.clevelandart.org/api/artworks", {params})
       .then((response) => {
         for (const artwork of response.data.data) {
@@ -70,8 +76,36 @@ function App() {
       });
   };
 
+  const loadDetails = async(url) => {
+    const getImages = await http(url)
+      .then((response) => {
+        // for (const artwork of response.data.data) {
+        //   let creator = artwork.creators.length > 0 ? artwork.creators[0].description : "Unknown";
+        //   const newImage = {
+        //     image: artwork.images.web.url,
+        //     id: artwork.id,
+        //     title: artwork.title,
+        //     creator: creator,
+        //     date: artwork.creation_date,
+        //     details: artwork.tombstone,
+        //   }
+        //   // const image = artwork.images.web.url;
+        //   // const id = artwork.id;
+        //   // const title = artwork.title;
+        //   // const creator = artwork.creators[0].description;
+        //   // const date = artwork.creation_date;
+        //   // const details = artwork.tombstone;
+        //   imageData.push(newImage);
+        // };
+        console.log(response);
+      })
+      .catch((e) => {
+        console.log("ERROR getting artwork data");
+        console.log(e);
+      });
+  }
+
   useEffect(() => {
-    // loadMuseum();
     loadCleveland();
   }, []);
 
@@ -93,10 +127,20 @@ function App() {
         <p>Loading...</p>
       )} */}
       {imagesOnLoad.map((img, i) => (
-        <img src={img.image} alt="Anyád" key={i} />
+        <div key={i} onClick={showArtworkDetails}>
+          <img src={img.image} alt="Anyád" key={i} />
+          <div className={isShown ? "miniArt" : "detailedArt"}>
+            <p>{img.title}</p>
+          </div>
+        </div>
+        // <img src={img.image} alt="Anyád" key={i} onClick={(e) => loadDetails(`https://openaccess-api.clevelandart.org/api/artworks/?q=${e.target.title}`)}/>
       ))}
     </div>
   );
 }
 
 export default App;
+
+/* 
+https://openaccess-api.clevelandart.org/api/artworks/{id}
+*/
