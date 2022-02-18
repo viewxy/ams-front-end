e9f361bc319939cc09ba244d532b5452bf339ad9
IstvanPongracz <pongracz88@gmail.com>
2022-02-18 20:12:03 +0100

    Probing Pagination attack

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,33 +1,47 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import http from "axios";
 
-const Home = ({setDetails}) => {
+const Home = ({ setDetails }) => {
   const [imagesOnLoad, setImagesOnLoad] = useState([]);
   const [keyword, setKeyword] = useState("");
+  const [skipCount, setSkipCount] = useState(0);
+  const [counter, setCounter] = useState(0);
+
   // const [isShown, setIsShown] = useState(true);
 
+  // const [page, setPage] = useState(0);
 
-  const [page, setPage] = useState(0);
-  
   const [isFetching, setIsFetching] = useState(false);
-  const [hasMore, setHasMore] = useState(true);
+  // const [hasMore, setHasMore] = useState(true);
 
   // Cleveland API
-  const loadCleveland = async(keyword) => {
+  const loadCleveland = async (keyword) => {
     let artworkData = [];
     const params = {
       q: keyword, // keyword from input
       limit: 20, // number of results
       has_image: 1, // it has an image
-      page: page
+      skip: skipCount,
+      // page: page,
     };
 
-    const getImages = await http("https://openaccess-api.clevelandart.org/api/artworks", {params})
+    const getImages = await http(
+      "https://openaccess-api.clevelandart.org/api/artworks",
+      { params }
+    )
       .then((response) => {
-        console.log(response.data.data)
+        // if (response.data.data.length === 0) {
+        //   setCounter(counter - 1);
+        //   setSkipCount(counter * 20);
+        //   return loadCleveland();
+        // }
+        console.log(response.data.data);
         for (const artwork of response.data.data) {
-          let creator = artwork.creators.length > 0 ? artwork.creators[0].description : "Unknown";
+          let creator =
+            artwork.creators.length > 0
+              ? artwork.creators[0].description
+              : "Unknown";
           const newArtwork = {
             image: artwork.images.web.url,
             id: artwork.id,
@@ -37,80 +51,114 @@ const Home = ({setDetails}) => {
             details: artwork.tombstone,
             funFact: artwork.fun_fact,
             technique: artwork.technique,
-          }
+          };
           artworkData.push(newArtwork);
 
-          setImagesOnLoad((prevImages) => {
-            return [...new Set([...prevImages, ...artworkData.map((b) => b.title)])];
-          });
-          setPage((prevPageNumber) => prevPageNumber + 1);
-          setHasMore(response.data.data > 0);
-          setIsFetching(false);
-
           // console.log(newArtwork)
-        };
-        setImagesOnLoad(artworkData);
-        
+        }
       })
       // .then((response) => {
-        // setImagesOnLoad(response.data.data);
+      // setImagesOnLoad((prevImages) => {
+      //   return [
+      //     ...new Set([...prevImages, ...artworkData.map((b) => b.title)]),
+      //   ];
+      // });
+      // setPage((prevPageNumber) => prevPageNumber + 1);
+      // setHasMore(response.data.data > 0);
+      // setIsFetching(false);
+      // setImagesOnLoad(response.data.data);
       // })
-      
+
       .catch((e) => {
         console.log("ERROR getting artwork data");
         console.log(e);
       });
-    };
+    setImagesOnLoad(artworkData);
+  };
 
-  function loadMoreItems() {
-    setIsFetching(true);
+  // function loadMoreItems() {
+  //   setIsFetching(true);
 
-    //mocking an API call
-    setTimeout(() => {
-      setImagesOnLoad((prevState) => [
-        ...prevState,
-        ...Array.from(Array(20).keys(), (n) => n + prevState.length + 1),
-      ]);
-      setIsFetching(false);
-    }, 2000);
-  }
+  //   //mocking an API call
+  //   setTimeout(() => {
+  //     setImagesOnLoad((prevState) => [
+  //       ...prevState,
+  //       ...Array.from(Array(20).keys(), (n) => n + prevState.length + 1),
+  //     ]);
+  //     setIsFetching(false);
+  //   }, 2000);
+  // }
 
   // const showArtworkDetails = () => {
   //   setIsShown((isShown) => !isShown);
   // };
 
   const storeDetails = (img) => {
-    setDetails(img) // local storage eseten ez mar lehet nem is kell, de talan jo lesz meg ez a http.post-hoz
-    localStorage.setItem('img', img.image)
-    localStorage.setItem('creator', img.creator)
-    localStorage.setItem('id', img.id)
-    localStorage.setItem('title', img.title)
-    localStorage.setItem('creation_date', img.date)
-    localStorage.setItem('technique', img.technique)
-    localStorage.setItem('funFact', img.funFact)
-    localStorage.setItem('search', keyword)
-  }
+    setDetails(img); // local storage eseten ez mar lehet nem is kell, de talan jo lesz meg ez a http.post-hoz
+    localStorage.setItem("img", img.image);
+    localStorage.setItem("creator", img.creator);
+    localStorage.setItem("id", img.id);
+    localStorage.setItem("title", img.title);
+    localStorage.setItem("creation_date", img.date);
+    localStorage.setItem("technique", img.technique);
+    localStorage.setItem("funFact", img.funFact);
+    localStorage.setItem("search", keyword);
+  };
+
+  let up = 0;
+  const pageCountUp = () => {
+    up++;
+    setCounter(up);
+    setSkipCount(counter * 20);
+    loadCleveland();
+    console.log(skipCount);
+  };
+
+  const pageCountDown = () => {
+    if (counter === 0) {
+      return setSkipCount(0);
+    }
+    setCounter(counter - 1);
+    setSkipCount(counter * 20);
+    loadCleveland();
+    console.log(skipCount);
+  };
 
   useEffect(() => {
+    pageCountUp();
+    pageCountDown();
     loadCleveland();
   }, []);
 
   return (
     <div>
-      <input placeholder='Search' type="text" value={keyword} onChange={(e) => setKeyword(e.target.value)}/>
+      <input
+        placeholder="Search"
+        type="text"
+        value={keyword}
+        onChange={(e) => setKeyword(e.target.value)}
+      />
       <button onClick={() => loadCleveland(keyword)}>KATTINTS</button>
-      <div className='main'>
-      {imagesOnLoad.map((img, i) => (
-        <div key={i}>
-          <Link to="/imageDetails"><img src={img.image} onClick={() => storeDetails(img)} alt="Anyád" /></Link>
-          <p>{img.title}</p>
-        </div>
-      ))}
+      <div className="main">
+        {imagesOnLoad.map((img, i) => (
+          <div key={i}>
+            <Link to="/imageDetails">
+              <img
+                src={img.image}
+                onClick={() => storeDetails(img)}
+                alt="Anyád"
+              />
+            </Link>
+            <p>{img.title}</p>
+          </div>
+        ))}
       </div>
-      {isFetching && <p>Fetching items...</p>}
-      {!isFetching && hasMore && <button onClick={loadMoreItems}>Load more</button>}
+      <button onClick={() => pageCountDown()}>&#9754;</button>
+      <button onClick={() => pageCountUp()}>&#9755;</button>
+      {/* {isFetching && <p>Fetching items...</p>}
+      {!isFetching && <button onClick={loadMoreItems}>Load more</button>} */}
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
